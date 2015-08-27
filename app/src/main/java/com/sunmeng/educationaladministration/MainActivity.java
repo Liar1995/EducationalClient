3606616415241ca3a6461995791fc01caabf15ec
Sunmeng <sunmeng1995@outlook.com>
2015-08-27 09:43:00 +0800

    删除没用的文件

--- app/src/main/java/com/sunmeng/educationaladministration/MainActivity.java
+++ app/src/main/java/com/sunmeng/educationaladministration/MainActivity.java
@@ -5,11 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.view.animation.Animation;
-import android.view.animation.ScaleAnimation;
 import android.widget.RelativeLayout;
-
-import com.sunmeng.educationaladministration.animation.MyImageView;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
